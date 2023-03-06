dea484bc6875a72a62bf9acbea3c11adc7d9b714
eunice-n <87886377+eunice-n@users.noreply.github.com>
2023-03-06 04:31:02 +0000

    Finished challenges 01_02 and 01_03

--- 01_restaurant/challenges/01_02_anniversary_ch.sql
+++ 01_restaurant/challenges/01_02_anniversary_ch.sql
@@ -1,7 +1,10 @@
 -- Create a table in the database to store customer
 -- responses to our anniversary invitation.
-
+CREATE TABLE AnniversaryAttendees (
+CustomerID INTEGER,
+PartySize INTEGER
+);
 -- Associate a customer's ID number with the number of people
 -- they plan to bring in their party.
-
+SELECT * FROM AnniversaryAttendees;
 -- Hint: SQLite offers the INTEGER and REAL datatypes
--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -1,5 +1,18 @@
 -- Create reports that will be used to make three menus.
 
 -- Create a report with all the items sorted by price (lowest to highest).
+SELECT * FROM 
+Dishes 
+ORDER BY Price ASC;
+
 -- Create a report showing appetizers and beverages.
+SELECT * FROM
+Dishes
+WHERE Type = 'Appetizer' OR Type = 'Beverage'
+ORDER BY Type;
+
 -- Create a report with all items except beverages.
+SELECT * FROM
+Dishes
+WHERE NOT Type = 'Beverage'
+ORDER By Type;
