415fca1324c4de293162100f7aea8f43e829a918
eunice-n <87886377+eunice-n@users.noreply.github.com>
2023-03-28 16:05:35 +0000

    Attempted 01_06 and 01_07

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,20 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+SELECT * FROM Reservations
+WHERE CustomerID == (SELECT CustomerID FROM Customers
+                      WHERE FirstName == "Norby") AND
+                      Date > "2022-07-24";
+
+
+-- Today is July 24, 2022.
+DELETE FROM Reservations
+WHERE CustomerID == (SELECT CustomerID FROM Customers
+                      WHERE FirstName == "Norby") AND
+                      Date > "2022-07-24";
+
+-- Solution used JOIN to solve the challenge:
+SELECT * FROM Reservations
+JOIN Customers ON Customers.CustomerID = Reservations.CustomerID
+WHERE Customers.FirstName == "Norby" AND
+Reservations.Date > "2022-01-01";
