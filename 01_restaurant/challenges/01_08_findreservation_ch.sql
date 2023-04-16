d51370229dd0deddf6f78347760622d8caaf81a6
eunice-n <87886377+eunice-n@users.noreply.github.com>
2023-04-16 07:53:08 +0000

    Attempted 08 and 09 challenges.

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,19 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT * FROM Reservations
+WHERE Date LIKE "%-06-14%" AND
+CustomerID == (SELECT CustomerID 
+                      FROM Customers
+                      WHERE FirstName LIKE "Ste%s%" OR
+                      LastName LIKE "Ste%s%") AND
+PartySize == 4;
+
+SELECT Customers.FirstName, Customers.LastName, 
+Reservations.Date, Reservations.PartySize, Reservations.ReservationID
+FROM Reservations
+JOIN Customers ON Customers.CustomerID = Reservations.CustomerID
+WHERE Customers.LastName LIKE "St%" AND
+Reservations.PartySize = 4 
+ORDER BY Reservations.Date DESC 
--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,27 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+SELECT *
+FROM Customers
+WHERE Email == 'smac@kinetecoinc.com';
+
+DELETE FROM Customers
+WHERE CustomerID == 103 AND CustomerID == 104;
+
+INSERT INTO Customers
+(FirstName, LastName, Email, Phone) VALUES
+('Sam', 'McAdams', 'smac@kinetecoinc.com', '(555) 555-1232');
+
+INSERT INTO Reservations
+(CustomerID, Date, PartySize) VALUES
+((SELECT CustomerID 
+FROM Customers 
+WHERE Email == 'smac@kinetecoinc.com'),
+'2022-08-12 18:00:00',
+5);
+
+SELECT Customers.FirstName, Customers.LastName, Reservations.Date, Reservations.PartySize
+FROM Reservations
+JOIN Customers ON Customers.CustomerID = Reservations.CustomerID
+WHERE Custoners.Email == 'smac@kinetecoinc.com';
