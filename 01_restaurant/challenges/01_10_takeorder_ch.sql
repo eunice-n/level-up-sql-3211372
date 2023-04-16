cc6ac7865860decae0aeafd05fa18f674d9f71a5
eunice-n <87886377+eunice-n@users.noreply.github.com>
2023-04-16 08:33:39 +0000

    attempted challenge 10 (failed)

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,46 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+-- Find Customer in Customer Record
+SELECT *
+FROM Customers
+WHERE FirstName == 'Loretta' AND LastName == 'Hundey';
+
+-- Create OrderID for this order
+INSERT INTO Orders (CustomerID, OrderDate)
+VALUES ((SELECT Customers.CustomerID
+          FROM Customers 
+          WHERE FirstName == 'Loretta' AND 
+                LastName == 'Hundey'), 
+        '2022-09-20 14:00:00');
+
+-- View added Order
+SELECT * FROM Orders ORDER BY OrderID DESC;
+
+-- View Ordered Dishes in one table
+SELECT DishID, Name, Price
+FROM Dishes WHERE
+Name == 'Mini Cheeseburgers' OR
+Name == 'House Salad' OR
+Name == 'Tropical Blue Smoothie';
+
+
+-- Add Dishes to OrderDishes
+INSERT INTO OrdersDishes (OrderID, DishID)
+VALUES (1001, 7);
+
+INSERT INTO OrdersDishes (OrderID, DishID)
+VALUES (1001, 4);
+
+INSERT INTO OrdersDishes (OrderID, DishID)
+VALUES (1001, 20);
+
+SELECT * FROM OrdersDishes WHERE OrderID == 1001;
+
+-- Calculate Price
+SELECT * -- SUM(Dishes.Price)
+FROM Orders WHERE Orders.OrderID == 1001
+JOIN Dishes ON Orders.DishID = Dishes.DishID;
+
+
