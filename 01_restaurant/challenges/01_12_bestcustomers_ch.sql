4e2473cdb4dddbf3c665df6898f14a512b3e904b
eunice-n <87886377+eunice-n@users.noreply.github.com>
2023-05-09 16:03:49 +0000

    Commit on 9 May

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,13 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+SELECT COUNT(Orders.OrderID) AS OrderCount,
+Orders.CustomerID, Customers.CustomerID, Customers.FirstName,
+Customers.LastName, Customers.Email
+FROM Orders
+JOIN Customers
+ON Customers.CustomerID = Orders.CustomerID
+GROUP BY Orders.CustomerID
+ORDER BY OrderCount DESC
+LIMIT 15;
--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,12 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+SELECT *
+FROM Books
+WHERE Title = 'Dracula';
+
+SELECT Books.BookID, Books.Title, Loans.BookID, Loans.ReturnedDate
+FROM Books
+JOIN Loans
+ON Books.BookID = Loans.BookID
+WHERE Books.Title = 'Dracula'
+AND ReturnedDate ISNULL;
